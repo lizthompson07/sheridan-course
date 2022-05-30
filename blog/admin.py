805b412b4b3829f27b4d63d126c2d155b41018eb
unknown <lizthompson07@gmail.com>
2022-05-29 21:10:46 -0300

    add admin models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'created',
+        'updated',
+    )
+
+
+admin.site.register(models.Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,12 @@ class Post(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
     updated = models.DateTimeField(auto_now=True)  # Updates on each save
+
+    class Meta:
+        # Sort by the `created` field. The `-` prefix
+        # specifies to order in descending/reverse order.
+        # Otherwise, it will be in ascending order.
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
