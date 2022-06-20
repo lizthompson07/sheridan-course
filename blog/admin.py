ef1ef47d3333b5e7e317c3672c5d087aab77bfee
unknown <lizthompson07@gmail.com>
2022-06-20 17:57:09 -0300

    update admin model

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,39 @@ from django.contrib import admin
 # Register your models here.
 
 from . import models
+from .models import Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Set default admin view for Comment
+    """
+    list_display = (
+        'name',
+        'text',
+        'approved',
+        'created',
+        'updated',
+    )
+
+    search_fields = (
+        'text',
+        'post__title',
+        'name__username',
+        'name__first_name',
+        'name__last_name',
+    )
+
+    list_filter = (
+        'approved',
+        )
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ('name', 'email', 'text', 'approved')
+    readonly_fields = ('name', 'email', 'text', )
+    extra = 0  # remove empty rows
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -35,6 +68,10 @@ class PostAdmin(admin.ModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
 
+    inlines = [
+        CommentInline,
+    ]
+
 
 @admin.register(models.Topic)
 class TopicAdmin(admin.ModelAdmin):
@@ -47,31 +84,6 @@ class TopicAdmin(admin.ModelAdmin):
     )
 
     prepopulated_fields = {'slug': ('name',)}
-
-
-class CommentAdmin(admin.ModelAdmin):
-    """
-    Set default admin view for Comment
-    """
-    list_display = (
-        'name',
-        'text',
-        'approved',
-        'created',
-        'updated',
-    )
-
-    search_fields = (
-        'text',
-        'post__title',
-        'name__username',
-        'name__first_name',
-        'name__last_name',
-    )
-
-    list_filter = (
-        'approved',
-        )
 
 
 admin.site.register(models.Post, PostAdmin)
